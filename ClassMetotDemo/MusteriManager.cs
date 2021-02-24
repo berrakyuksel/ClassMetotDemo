4ff8ea12b7672ab1270e173eeb00be60124b125c
Çağıl Berrak Yüksel <cagilberrak@gmail.com>
2021-02-24 13:12:25 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad+" "+musteri.SoyAd+" isimli müşteri eklenmiştir.");
+        }
+
+        public void listele(Musteri[] musteri)
+        {
+            foreach (var musteriler in musteri)
+            {
+                Console.WriteLine(musteriler.Ad+" "+musteriler.SoyAd);
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            Musteri musteri2 = new Musteri();
+            Musteri musteri3 = new Musteri();
+
+
+
+            musteri1.Ad = "Berrak";
+            musteri1.SoyAd = "Yuksel";
+            musteri1.ID = 0;
+
+            musteri2.Ad = "Neşe";
+            musteri2.SoyAd = "Yuksel";
+            musteri2.ID = 1;
+
+            musteri3.Ad = "Ömür";
+            musteri3.SoyAd = "Yuksel";
+            musteri3.ID = 2;
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            musteriManager.Ekle(musteri1);
+            musteriManager.listele(musteriler);
+
+
+
+
+
+
+        }
+    }
+}
